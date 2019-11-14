66ae420a2ce6d6c029d748979f7ec37eeb264e61
Nirayka Monga <NMonga@ucvts.tec.nj.us>
2019-11-14 10:07:38 -0500

    aaaa

--- src/sum.js
+++ src/sum.js
@@ -1,20 +1,33 @@
 
 const readlineSync = require("readline-sync");
-let lowerBound;
-let upperBound;
-let enteredLower;
-let enteredUpper;
-
-do {
-  enteredLower = readlineSync.question("\nLower bound: ");
-  enteredUpper = readlineSync.question("Upper bound: ");
-  lowerBound = Number(enteredLower)
-  upperBound = Number(enteredUpper)
-} while (lowerBound >= upperBound)
-
-finalDisplay = [((upperBound-lowerBound) + 1) / 2] * (upperBound + lowerBound)
-console.log("\n"finalDisplay)
+const MIN = Number.MIN_SAFE_INTEGER
+const MAX = Number.MAX_SAFE_INTEGER
+let lowerBound = Number(readlineSync.question("\nLower bound: "));
+let upperBound = Number(readlineSync.question("Upper bound: "));
+let lastEven;
+let firstEven;
 
 
+while ((upperBound < MIN) || (lowerBound > upperBound) || (lowerBound < MIN) || (lowerBound > MAX) || (upperBound > MAX) || (!(Number.isInteger(upperBound)))
+|| (!(Number.isInteger(lowerBound))) || (Number.isNaN(lowerBound)) || (Number.isNaN(upperBound))) {
+  lowerBound = Number(readlineSync.question("Lower bound: "));
+  upperBound = Number(readlineSync.question("Upper bound: "));
+}
 
-// FIX THE MATH PART!
+
+if (upperBound % 2 === 0) {
+  lastEven = upperBound
+} else {
+  lastEven = upperBound - 1
+}
+
+if (lowerBound % 2 === 0) {
+  firstEven = lowerBound
+} else {
+  firstEven = lowerBound + 1
+}
+
+let numberForFormula = [(firstEven + lastEven) / 2] - 1
+let finalDisplay = numberForFormula * (numberForFormula + 1)
+
+console.log("\n" + finalDisplay + "\n")
